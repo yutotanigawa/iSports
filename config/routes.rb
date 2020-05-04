3e09d7c8840e5f20beb463c26db91c76a9c60b88
yutotanigawa <yunicipal@gmail.com>
2020-05-04 08:51:22 +0000

    [Add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # admin側のルーティング
+  devise_for :admins, controllers:{
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   # user側のルーティング
+  devise_for :users, controllers:{
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   root 'homes#top'
 end
