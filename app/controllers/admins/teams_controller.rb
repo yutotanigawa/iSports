a00d273ff5b652b9aec5e9571ba3099d97fc7616
yutotanigawa <yunicipal@gmail.com>
2020-05-10 15:01:26 +0000

    [Add]admins/teams-index,show,update

--- app/controllers/admins/teams_controller.rb
+++ app/controllers/admins/teams_controller.rb
@@ -1,13 +1,26 @@
 class Admins::TeamsController < ApplicationController
     def index
+        @teams = Team.all
     end
 
     def show
+        @team = Team.find(params[:id])
     end
 
     def update
+        @team = Team.find(params[:id])
+        @team.update(team_params)
+        redirect_to admins_team_path(@team)
     end
 
     def destroy
+        @team = Team.find(params[:id])
+        @team.destroy
+        redirect_to admins_team_path
+    end
+
+    private
+    def team_params
+        params.require(:team).permit(:publication_status)
     end
 end
