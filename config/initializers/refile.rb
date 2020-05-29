d09b5c4bf9956eb172babc0edecada06d9dc7259
yutotanigawa <yunicipal@gmail.com>
2020-05-29 09:53:35 +0000

    [CommentOut]config/initializers/refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
-require "refile/s3"
+# require "refile/s3"
 
-aws = {
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: "ap-northeast-1",
-  bucket: "isports-s3",
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+# aws = {
+#   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#   region: "ap-northeast-1",
+#   bucket: "isports-s3",
+# }
+# Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+# Refile.store = Refile::S3.new(prefix: "store", **aws)
